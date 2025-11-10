9b0fecd3cbde1697d2abbca71ece83e19171181f
Mahmoud Ammar <80252748+MahmoudAziz-Ammar@users.noreply.github.com>
2025-11-10 22:19:24 +0100

    1 st commit

--- main.py
+++
@@ -1,7 +0,0 @@
-from app.rag_pipeline import query_huggingface
-
-if __name__ == "__main__":
-    question = "Quels sont les compétences techniques de John W. Smith ?"
-    response = query_huggingface(question)
-    print("Réponse générée :")
-    print(response)
